82ab81547b61b3cb6215533a0cd7ce2cb650afe1
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-29 08:25:01 -0600

    added controllers

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ console.log('test')
 
 angular
   .module('musicAngular', ['ngRoute'])
+  .config(($routeProvider) => {
+    $routeProvider
+      .when('/', {
+        controller: 'HomeCtrl',
+        templateUrl: '/partials/home.html',
+       })
+      .when('/profile', {
+        controller: 'ProfileCtrl',
+        templateUrl: '/partials/profile.html'
+      })
+   })
